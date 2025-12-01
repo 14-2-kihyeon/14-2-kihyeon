c5a72bc4e19e1e0a0ff0f44b66c7d40a47e6bc72
14-2-kihyeon <tjrlgus19@gmail.com>
2025-12-01 21:32:48 +0900

    Title: 2차원 최대 증가 수열, Time: 56ms, Memory: 17MB, Status: Passed - Codetree

--- 251201/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
+++ 251201/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -1,49 +1,30 @@
-import sys
-
-# INT_MIN을 사용하여 최댓값을 쉽게 찾을 수 있도록 합니다.
-INT_MIN = -sys.maxsize
-
-# 변수 선언 및 입력:
-# N: 행의 수, M: 열의 수
 n, m = map(int, input().split())
-grid = [
-    list(map(int, input().split()))
-    for _ in range(n)
-]
 
-# DP 배열 정의: dp[i][j] = (i, j)에 도착했을 때 밟을 수 있는 최대 칸 수
-# N x M 크기로 초기화합니다.
-dp = [
-    [0] * m
-    for _ in range(n)
-]
+grid = [list(map(int, input().split())) for _ in range(n)]
 
-# 초기 조건: 시작점 (0, 0)은 1칸을 밟은 상태입니다.
+dp = [[0] * m for _ in range(n)]
+
+# for i in range(n):
+#     for j in range(m):
+#         dp[i][j] = float('-inf')
 dp[0][0] = 1
-
 
 for i in range(n):
     for j in range(m):
 
-        if dp[i][j] == 0:
-            continue
-            
-        for a in range(i, n):
-            for b in range(j, m):
-                
+        for k in range(i):
+            for l in range(j):
 
-                if a == i and b == j:
+                if dp[k][l] == 0:
                     continue
                 
+                if grid[k][l] < grid[i][j]:
+                    dp[i][j] = max(dp[i][j], dp[k][l] + 1)
 
-                if grid[i][j] < grid[a][b]:
-                    dp[a][b] = max(dp[a][b], dp[i][j] + 1)
+ans = float('-inf')
 
-
-ans = INT_MIN
 for i in range(n):
     for j in range(m):
         ans = max(ans, dp[i][j])
-
 
 print(ans)
